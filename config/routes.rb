66cb7f7bbe39078f1032368284f1dc9aa5351f80
Pedrohrn <dev14.seucondominio@gmail.com>
2019-06-20 17:44:47 -0300

    liberando novas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'passagem_servicos#index'
 
-  resources :passagem_servicos, 	only: [:index, :show, :create, :destroy, :update]
-  resources :perfis, 							only: [:index, :show, :create, :update, :destroy]
-  resources :categorias, 					only: [:index, :show, :create, :destroy, :update]
+  resources :passagem_servicos, 	only: [:index, :show, :create, :destroy, :update, :micro_update]
+  resources :perfis, 							only: [:index, :show, :create, :destroy, :update, :micro_update]
+  resources :categorias, 					only: [:index, :show, :create, :destroy, :update, :micro_update]
 end
